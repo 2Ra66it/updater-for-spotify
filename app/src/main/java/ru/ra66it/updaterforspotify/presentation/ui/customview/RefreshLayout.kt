e3f12de70813247a407fd17d66c76af730b93a5f
2Ra66it <andrejj-Klepikov98@rambler.ru>
2019-04-04 20:24:55 +0300

    Fixed crash

--- app/src/main/java/ru/ra66it/updaterforspotify/presentation/ui/customview/RefreshLayout.kt
+++ app/src/main/java/ru/ra66it/updaterforspotify/presentation/ui/customview/RefreshLayout.kt
@@ -860,8 +860,6 @@ class RefreshLayout @JvmOverloads constructor(context: Context, attrs: Attribute
         clearAnimation()
 
         if (computeAnimateToStartDuration(from.toFloat()) <= 0) {
-            listener.onAnimationStart(null)
-            listener.onAnimationEnd(null)
             return
         }
 
