62bcf9d667931d78fe57b1d388472e548d0b42f3
2Ra66it <andrejj-Klepikov98@rambler.ru>
2019-08-18 17:44:58 +0300

    Cleaned constants Removed icons

--- app/src/main/java/ru/ra66it/updaterforspotify/Constants.kt
+++ app/src/main/java/ru/ra66it/updaterforspotify/Constants.kt
@@ -1,15 +1,10 @@
 package ru.ra66it.updaterforspotify
 
-
 const val spotifyPackage = "com.spotify.music"
-const val notificationIdKey = "notificationIdKey"
-const val latestLinkKey = "latestLinkKey"
-const val latestVersionKey = "latestVersionKey"
 const val notificationChanelId = "ufsChanelId"
 const val jobId = 322
 const val notificationId = 228
 const val sharedPreferencesName = "ufsPreferences"
-const val actionDownload = "actionDownload"
 const val spotifyNotInstalled = 0
 const val spotifyHaveUpdate = 1
 const val spotifyIsLatest = 2
